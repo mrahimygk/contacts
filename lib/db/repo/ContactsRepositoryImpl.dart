8498c00fcbbd485128a929a46dc59ddc203d33cf
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 11:15:05 +0330

    inserting to db : replace on conflict,

--- lib/db/repo/ContactsRepositoryImpl.dart
+++ lib/db/repo/ContactsRepositoryImpl.dart
@@ -3,11 +3,12 @@ import 'package:flutter_firstsourceio/db/dao/ContactsDao.dart';
 import 'package:flutter_firstsourceio/db/provider/DatabaseProvider.dart';
 import 'package:flutter_firstsourceio/db/repo/ContactsRepository.dart';
 import 'package:dio/dio.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class ContactsRepositoryImpl implements ContactsRepository {
   final dao = ContactsDao();
 
-  final contactsPerPage= 10;
+  final contactsPerPage = 10;
   int contactsPage = 1;
 
   final dio = Dio(
@@ -58,30 +59,30 @@ class ContactsRepositoryImpl implements ContactsRepository {
   Future<List<Contact>> getContactsFromNetwork() async {
     final response = await dio.get(CONTACTS_URL,
         queryParameters: {"page": contactsPage++, "row": contactsPerPage});
-    print(response);
     var data = List<Contact>();
     for (var value in response.data['data']) {
-      print(value);
-      data.add(Contact.fromMap(value));
+      final c = Contact.fromMap(value);
+      insert(c);
+      data.add(c);
     }
 
-    return null;
+    return data;
   }
 
   @override
   Future<Contact> insert(Contact data) async {
     final db = await databaseProvider.db();
-    data.id = await db.insert(contactsTable, data.toMap());
+    data.id = await db.insert(contactsTable, dao.toMap(data), conflictAlgorithm: ConflictAlgorithm.replace);
     return data;
   }
 
   @override
   Future<Contact> update(Contact data) async {
     final db = await databaseProvider.db();
-    await db.update(contactsTable, data.toMap(),
+    await db.update(contactsTable, dao.toMap(data),
         where: '$contactsColumnId = ?', whereArgs: [data.id]);
     return data;
   }
 }
 
-const CONTACTS_URL = "https://mock-rest-api-server.herokuapp.com/api/v1/user/";
+const CONTACTS_URL = "https://mock-rest-api-server.herokuapp.com/api/v1/user";
