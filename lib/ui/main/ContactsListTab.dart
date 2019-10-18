52e3edf3898777c816eacd01a4d69b116cc78e83
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 11:14:30 +0330

    calling get contacts from net on every init state

--- lib/ui/main/ContactsListTab.dart
+++ lib/ui/main/ContactsListTab.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firstsourceio/db/provider/DatabaseProvider.dart';
+import 'package:flutter_firstsourceio/db/repo/ContactsRepositoryImpl.dart';
 
 class ContactsListTab extends StatefulWidget {
   _ContactsListTabState createState() => _ContactsListTabState();
 }
 
 class _ContactsListTabState extends State<ContactsListTab> {
+
+  final cRepo = ContactsRepositoryImpl(DatabaseProvider.get);
+
+  @override
+  void initState() {
+    cRepo.getContactsFromNetwork();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
