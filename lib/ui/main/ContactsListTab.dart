fd0b7f7bf5f27ff642ec15953ee7748221d3d447
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 11:46:10 +0330

    loading into basic widget and managing scroll

--- lib/ui/main/ContactsListTab.dart
+++ lib/ui/main/ContactsListTab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firstsourceio/data/Contact.dart';
 import 'package:flutter_firstsourceio/db/provider/DatabaseProvider.dart';
 import 'package:flutter_firstsourceio/db/repo/ContactsRepositoryImpl.dart';
 
@@ -7,20 +8,52 @@ class ContactsListTab extends StatefulWidget {
 }
 
 class _ContactsListTabState extends State<ContactsListTab> {
-
   final cRepo = ContactsRepositoryImpl(DatabaseProvider.get);
+  ScrollController scrollController;
 
   @override
   void initState() {
     cRepo.getContactsFromNetwork();
+    scrollController = ScrollController();
+    scrollController.addListener(_scrollListener);
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ContactsListTab'),
-    );
+    return FutureBuilder<List<Contact>>(
+        future: cRepo.getContactsFromDb(),
+        builder: (context, inStream) {
+          if (!inStream.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            controller: scrollController,
+            padding: EdgeInsets.all(6.0),
+            children: inStream.data
+                .map((contact) => Dismissible(
+                      key: Key(contact.hashCode.toString()),
+                      child: Card(
+                        child: ListTile(
+                          title: Text(contact.firstName),
+                        ),
+                      ),
+                    ))
+                .toList(),
+          );
+        });
+  }
+
+  _scrollListener() {
+    if (scrollController.offset >= scrollController.position.maxScrollExtent &&
+        !scrollController.position.outOfRange) {
+      print('reached the end: asking new items');
+      cRepo.getContactsFromNetwork().then((onValue) {
+        setState(() {});
+      });
+    }
   }
 }
