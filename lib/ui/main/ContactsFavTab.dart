3260f912e8b91b4b06b7d934a1e9e70b6c035008
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 09:01:21 +0330

    + bottom nav tab for fav list.

---
+++ lib/ui/main/ContactsFavTab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContactsFavTab extends StatefulWidget {
+  _ContactsFavTabState createState() => _ContactsFavTabState();
+}
+
+class _ContactsFavTabState extends State<ContactsFavTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('ContactsFavTab'),
+    );
+  }
+}
