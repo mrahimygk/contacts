6a3474efeb9c1fecba0aa1f56bbcd55b0399b7d3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 12:05:17 +0330

    adding a new widget for contacts

---
+++ lib/ui/widgets/ContactsCard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_firstsourceio/data/Contact.dart';
+import 'package:flutter/material.dart';
+
+class ContactsCard extends StatelessWidget {
+  final Contact contact;
+
+  const ContactsCard({Key key, this.contact}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(contact.firstName),
+      ),
+    );
+  }
+}
