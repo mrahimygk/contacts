5d728a0bf5b7f67213e1cf1a2dada1d471b32dc3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 09:00:51 +0330

    + bottom nav tab for contacts list.

---
+++ lib/ui/main/ContactsListTab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContactsListTab extends StatefulWidget {
+  _ContactsListTabState createState() => _ContactsListTabState();
+}
+
+class _ContactsListTabState extends State<ContactsListTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('ContactsListTab'),
+    );
+  }
+}
