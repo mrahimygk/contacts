d26280742dc946a80e4c5ca39ea6e7234bab418d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 16:12:01 +0330

    + args class to capsulate contact

---
+++ lib/data/args/AddContactArgs.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_firstsourceio/data/model/Contact.dart';
+
+class AddContactArgs {
+  final int index;
+  final Contact contact;
+
+  AddContactArgs(this.index, this.contact);
+}
