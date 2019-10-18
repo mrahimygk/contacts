b8dc20d2af52e850bf08c869ce240b17a5ad77ec
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 08:29:12 +0330

    using moving contacts from the pages package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/ContactsListPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,31 +16,4 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-}
-
-class ContactsListPage extends StatefulWidget {
-  ContactsListPage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _ContactsListPageState createState() => _ContactsListPageState();
-}
-
-class _ContactsListPageState extends State<ContactsListPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addContact,
-        tooltip: 'Add Contact',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _addContact() {}
 }
