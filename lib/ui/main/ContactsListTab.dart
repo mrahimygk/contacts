a4f21bb2c6ece0e38529d7461ccd1c8f71eeb738
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-18 14:30:15 +0330

    impling search in a dirty way

--- lib/ui/main/ContactsListTab.dart
+++ lib/ui/main/ContactsListTab.dart
@@ -11,13 +11,29 @@ class ContactsListTab extends StatefulWidget {
 class _ContactsListTabState extends State<ContactsListTab> {
   final contactsRepo = ContactsRepositoryImpl(DatabaseProvider.get);
   ScrollController scrollController;
+  String filter;
+
+  final TextEditingController contactsSearchController =
+      TextEditingController();
 
   @override
   void initState() {
     contactsRepo.getContactsFromNetwork();
     scrollController = ScrollController();
     scrollController.addListener(_scrollListener);
+    contactsSearchController.addListener(() {
+      setState(() {
+        filter = contactsSearchController.text;
+      });
+    });
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    contactsSearchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -37,8 +53,11 @@ class _ContactsListTabState extends State<ContactsListTab> {
               Padding(
                 padding: const EdgeInsets.only(left: 18.0, right: 18.0),
                 child: TextField(
+                  controller: contactsSearchController,
+                  onChanged: (value) {},
                   maxLines: 1,
                   decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
                     hintText: 'Type to Search...',
                   ),
                 ),
@@ -49,47 +68,104 @@ class _ContactsListTabState extends State<ContactsListTab> {
                   controller: scrollController,
                   padding: EdgeInsets.all(6.0),
                   children: data
-                      .map((contact) => ContactsCard(
-                            contact: contact,
-                            onEdit: (c) {
-                              print('editing $c');
-                            },
-                            onRemove: (theRemovingContact) {
-                              contactsRepo
-                                  .removeContactApi(theRemovingContact)
-                                  .then((f) {
+                      .map((contact) => filter == null || filter == ""
+                          ? ContactsCard(
+                              contact: contact,
+                              onEdit: (c) {
+                                print('editing $c');
+                              },
+                              onRemove: (theRemovingContact) {
                                 contactsRepo
-                                    .delete(theRemovingContact)
-                                    .then((cn) {
-                                  setState(() {});
-                                  Scaffold.of(context).showSnackBar(SnackBar(
-                                    content: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          child: Text(
-                                              'Contact ${theRemovingContact.firstName} ${theRemovingContact.lastName} removed'),
-                                        ),
-                                        Expanded(
-                                          child: FlatButton(
-                                            child: Text('Undo'),
-                                            onPressed: () {
-                                              contactsRepo
-                                                  .insert(theRemovingContact)
-                                                  .then((g) {
-                                                contactsRepo.insertApi(
-                                                    theRemovingContact);
-                                              });
-                                              setState(() {});
-                                            },
+                                    .removeContactApi(theRemovingContact)
+                                    .then((f) {
+                                  contactsRepo
+                                      .delete(theRemovingContact)
+                                      .then((cn) {
+                                    setState(() {});
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                      content: Row(
+                                        children: <Widget>[
+                                          Expanded(
+                                            child: Text(
+                                                'Contact ${theRemovingContact.firstName} ${theRemovingContact.lastName} removed'),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ));
+                                          Expanded(
+                                            child: FlatButton(
+                                              child: Text('Undo'),
+                                              onPressed: () {
+                                                contactsRepo
+                                                    .insert(theRemovingContact)
+                                                    .then((g) {
+                                                  contactsRepo.insertApi(
+                                                      theRemovingContact);
+                                                });
+                                                setState(() {});
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ));
+                                  });
                                 });
-                              });
-                            },
-                          ))
+                              },
+                            )
+                          : contact.firstName
+                                      .toLowerCase()
+                                      .contains(filter.toLowerCase()) ||
+                                  contact.lastName
+                                      .toLowerCase()
+                                      .contains(filter.toLowerCase()) ||
+                                  contact.email
+                                      .toLowerCase()
+                                      .contains(filter.toLowerCase()) ||
+                                  contact.phoneNo
+                                      .toLowerCase()
+                                      .contains(filter.toLowerCase())
+                              ? ContactsCard(
+                                  contact: contact,
+                                  onEdit: (c) {
+                                    print('editing $c');
+                                  },
+                                  onRemove: (theRemovingContact) {
+                                    contactsRepo
+                                        .removeContactApi(theRemovingContact)
+                                        .then((f) {
+                                      contactsRepo
+                                          .delete(theRemovingContact)
+                                          .then((cn) {
+                                        setState(() {});
+                                        Scaffold.of(context)
+                                            .showSnackBar(SnackBar(
+                                          content: Row(
+                                            children: <Widget>[
+                                              Expanded(
+                                                child: Text(
+                                                    'Contact ${theRemovingContact.firstName} ${theRemovingContact.lastName} removed'),
+                                              ),
+                                              Expanded(
+                                                child: FlatButton(
+                                                  child: Text('Undo'),
+                                                  onPressed: () {
+                                                    contactsRepo
+                                                        .insert(
+                                                            theRemovingContact)
+                                                        .then((g) {
+                                                      contactsRepo.insertApi(
+                                                          theRemovingContact);
+                                                    });
+                                                    setState(() {});
+                                                  },
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ));
+                                      });
+                                    });
+                                  },
+                                )
+                              : Container())
                       .toList(),
                 ),
               ),
@@ -106,5 +182,27 @@ class _ContactsListTabState extends State<ContactsListTab> {
         setState(() {});
       });
     }
+  }
+}
+
+class ContactSearch extends SearchDelegate<Contact> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return null;
   }
 }
